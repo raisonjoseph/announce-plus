6ead9b8471c61fcb28a02f4e631b222062989429
Raison Joseph <raisonjosephau@gmail.com>
2026-03-21 07:56:31 +0530

    Remove temporary session reset endpoint

--- app/routes/api.reset-sessions.jsx
+++
@@ -1,7 +0,0 @@
-import { json } from "@remix-run/node";
-import prisma from "../db.server";
-
-export async function loader() {
-  const deleted = await prisma.session.deleteMany({});
-  return json({ deleted: deleted.count, message: "All sessions cleared. Reinstall the app." });
-}
